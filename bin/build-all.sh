6d4160a6a1ab67518bb0541cb04786fe636407bd
ArthurHlt <arthur.halet@orange.com>
2019-02-10 23:36:43 +0100

    rework build

--- bin/build-all.sh
+++ bin/build-all.sh
@@ -1,12 +1,39 @@
+#!/usr/bin/env bash
 #!/bin/bash
 
 set -e
 
-OUTDIR=$(dirname $0)/../out
-BINARYNAME="terraform-secure-backend"
+BASE=$(dirname $0)
+OUTDIR=${BASE}/../out
+BINARYNAME=terraform-secure-backend
+CWD=$(pwd)
+version=$(echo "${TRAVIS_BRANCH:-dev}" | sed 's/^v//')
 
-GOARCH=amd64 GOOS=windows $(dirname $0)/build $1 && cp $OUTDIR/$BINARYNAME "$OUTDIR/${BINARYNAME}_windows_amd64.exe"
-GOARCH=386 GOOS=windows $(dirname $0)/build $1 && cp $OUTDIR/$BINARYNAME "$OUTDIR/${BINARYNAME}_windows_386.exe"
-GOARCH=amd64 GOOS=linux $(dirname $0)/build $1 && cp $OUTDIR/$BINARYNAME "$OUTDIR/${BINARYNAME}_linux_amd64"
-GOARCH=386 GOOS=linux $(dirname $0)/build $1 && cp $OUTDIR/$BINARYNAME "$OUTDIR/${BINARYNAME}_linux_386"
-GOARCH=amd64 GOOS=darwin $(dirname $0)/build $1 && cp $OUTDIR/$BINARYNAME "$OUTDIR/${BINARYNAME}_darwin_amd64"
+function build {
+  local arch=$1; shift
+  local os=$1; shift
+  local ext=""
+
+  if [ "${os}" == "windows" ]; then
+      ext=".exe"
+  fi
+
+  cd ${CWD}
+  echo "building gobis-server (${os} ${arch})..."
+  GOARCH=${arch} GOOS=${os} go build -ldflags="-s -w -X main.Version=${version}" -o $OUTDIR/${BINARYNAME}_${os}_${arch}${ext} || {
+    echo >&2 "error: while building ${BINARYNAME} (${os} ${arch})"
+    return 1
+  }
+
+  echo "zipping gobis-server (${os} ${arch})..."
+  cd $OUTDIR
+  zip "${BINARYNAME}_${os}_${arch}.zip" "${BINARYNAME}_${os}_${arch}${ext}" || {
+    echo >&2 "error: cannot zip file ${BINARYNAME}_${os}_${arch}${ext}"
+    return 1
+  }
+  cd ${CWD}
+}
+
+build amd64 windows
+build amd64 linux
+build amd64 darwin
